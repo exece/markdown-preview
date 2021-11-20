37410d980358e0d846af825b1bcb1ef2c1d3b9d8
exece <63699551+exece@users.noreply.github.com>
2021-11-20 12:26:47 +1000

    Updated readme with img

---
+++ README.md
@@ -0,0 +1,3 @@
+# markdown-preview
+
+![Screenshot of website](https://raw.githubusercontent.com/exece/mark-down/main/markdown-preview.jpg)
--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 function App() {
-  const [markdown, setMarkdown] = useState("## markdown preview");
+  const markdownDefault = `
+  ## markdown preview 
+  > This is super cool. 
+  ### hello world 
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dapibus nisl sem, sed congue arcu semper sit amet. Pellentesque tempor sit amet odio eu blandit. Sed a commodo odio, at vehicula urna. Cras et sodales ante. Sed iaculis convallis laoreet. Mauris volutpat eros vel erat convallis, mattis rhoncus lectus ornare. Suspendisse scelerisque, lorem eu porta consequat, nibh nulla aliquet ipsum, sed facilisis dui lorem a arcu. Suspendisse vel lectus ut arcu facilisis porta. Aliquam lobortis hendrerit purus at rhoncus. Vestibulum mattis nunc faucibus mattis pellentesque. Duis quis elit nunc. `;
+  
+  const [markdown, setMarkdown] = useState(markdownDefault);
   return (
     <main>
       <section className="markdown">
@@ -11,7 +17,9 @@ function App() {
           value={markdown}
           onChange={(e) => setMarkdown(e.target.value)}
         ></textarea>
-        <article className="result"><ReactMarkdown>{markdown}</ReactMarkdown></article>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
       </section>
     </main>
   );
